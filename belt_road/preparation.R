f0e381b7fa79c6c02eed114e72d16ae3c373a516
Zhiliang Lin <991788890@qq.com>
2020-02-16 23:46:28 +0800

    把一带一路数据准备好

--- belt_road/preparation.R
+++ belt_road/preparation.R
@@ -2,7 +2,19 @@ library(pacman)
 p_load(data.table,tidyverse,readxl,xlsx)
 
 
+
 here()
+
+# 选出一带一路相关的数据 -------------------------------------------------------------
+imdb_br_long <- 
+  imdb_long %>% 
+  filter(country %in% br_vars) %>% 
+  .[,c(1:34)] %>% 
+  lazy_dt()
+
+
+dim(imdb_br_long)
+
 
 # 一带一路国家名称 ----------------------------------------------------------------
 br_vars <- read_excel("../data/一带一路国家名称.xlsx",sheet = "Sheet1") %>% 
@@ -15,11 +27,17 @@ br_vars <- read_excel("../data/一带一路国家名称.xlsx",sheet = "Sheet1") 
 # 一带一路国家参与的电影的所有tt_num ----------------------------------------------------
 
 tt_br <- 
-  imdb_belt_road_long %>% 
+  imdb_br_long %>% 
   group_by(tt_num) %>% 
-  summarise(n = n())
+  summarise(n = n()) %>% 
+  arrange(desc(n)) %>% 
+  as_tibble()
 
 
+tt_br_vars <- 
+  tt_br %>% 
+  .[1] %>% 
+  as_vector()
 
 
 
@@ -32,13 +50,13 @@ tt_br_alone <-
   .[1] %>% 
   as_vector()
 
+tt_br_alone_vars <- tt_br_alone
 
 # ## “一带一路”沿线国家🐟合作🐟的电影的tt_num -------------------------------------------
-
-
-
 tt_br_col <- 
   tt_br %>% 
   filter(n != 1) %>% 
   .[1] %>% 
   as_vector()
+
+tt_br_col_vars <- tt_br_col
